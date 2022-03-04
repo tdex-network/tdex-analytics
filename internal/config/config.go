df74a29b275c4cc1a58f514991b39b3f4d38cb5c
sekulicd <sekula87@gmail.com>
2022-03-04 12:08:00 +0100

    comment config vars

--- internal/config/config.go
+++ internal/config/config.go
@@ -7,24 +7,41 @@ import (
 
 const (
 	// GrpcServerPortKey is port on which grpc server is running
-	GrpcServerPortKey       = "SERVER_PORT"
-	InfluxDbOrg             = "INFLUXDB_INIT_ORG"
-	InfluxDbAuthToken       = "INFLUXDB_TOKEN"
-	InfluxDbUrl             = "INFLUXDB_URL"
+	GrpcServerPortKey = "SERVER_PORT"
+	// InfluxDbOrg is organisation name
+	InfluxDbOrg = "INFLUXDB_INIT_ORG"
+	// InfluxDbAuthToken is token used as cred by tdexad
+	InfluxDbAuthToken = "INFLUXDB_TOKEN"
+	// InfluxDbUrl is db url
+	InfluxDbUrl = "INFLUXDB_URL"
+	// InfluxDbAnalyticsBucket is bucket that tdexd using to store data
 	InfluxDbAnalyticsBucket = "INFLUXDB_INIT_BUCKET"
-	DbUserKey               = "DB_USER"
-	DbPassKey               = "DB_PASS"
-	DbHostKey               = "DB_HOST"
-	DbPortKey               = "DB_PORT"
-	DbNameKey               = "DB_NAME"
-	DbMigrationPath         = "DB_MIGRATION_PATH"
-	DbInsecure              = "DB_INSECURE"
-	AwsRegion               = "AWSREGION"
-	TorProxyUrl             = "TOR_PROXY_URL"
-	RegistryUrl             = "REGISTRY_URL"
-	LogLevelKey             = "LOG_LEVEL"
-	PriceAmount             = "PRICE_AMOUNT"
-	JobPeriodInMinutes      = "JOB_PERIOD_IN_MINUTES"
+	// DbUserKey is postgres db user used by tdexd
+	DbUserKey = "DB_USER"
+	// DbPassKey is postgres db pass used by tdexd
+	DbPassKey = "DB_PASS"
+	// DbHostKey is postgres db host
+	DbHostKey = "DB_HOST"
+	// DbPortKey is postgres db port
+	DbPortKey = "DB_PORT"
+	// DbNameKey is postgres db name
+	DbNameKey = "DB_NAME"
+	// DbMigrationPath is postgres db migration path
+	DbMigrationPath = "DB_MIGRATION_PATH"
+	// DbInsecure is bool based on which postgres db conn will be created to AWS RDS or self-hosted PG DB
+	DbInsecure = "DB_INSECURE"
+	// AwsRegion region, used for connectiog to AWS RDS
+	AwsRegion = "AWSREGION"
+	// TorProxyUrl is tor client proxy url used to connect to liquidity providers using onion
+	TorProxyUrl = "TOR_PROXY_URL"
+	// RegistryUrl is url with info about available liquidity providers
+	RegistryUrl = "REGISTRY_URL"
+	// LogLevelKey is log level used by tdexa
+	LogLevelKey = "LOG_LEVEL"
+	// PriceAmount is amount used when invoking tdex-daemon MarketPrice RPC
+	PriceAmount = "PRICE_AMOUNT"
+	// JobPeriodInMinutes is recurring interval for running fetch balance/price jobs
+	JobPeriodInMinutes = "JOB_PERIOD_IN_MINUTES"
 )
 
 var vip *viper.Viper
