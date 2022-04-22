e74d21dfb71a7dd7ecf976eef1bab4ef16017f95
sekulicd <sekula87@gmail.com>
2022-04-22 12:51:24 +0200

    test fix

--- pkg/explorer/explorer_test.go
+++ pkg/explorer/explorer_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func Test_explorerService_GetAssetCurrency(t *testing.T) {
-	exp := NewExplorerService()
+	exp := NewExplorerService("https://blockstream.info/liquid/api/")
 	currency, err := exp.GetAssetCurrency(context.Background(), "b3f5ed2913486826fc58b35e9ae951c1680be6959c6f205c3cbc7fedb36d6b8a")
 	if err != nil {
 		t.Error(err)
