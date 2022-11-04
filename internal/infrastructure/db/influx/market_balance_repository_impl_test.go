d94f9765f9e466fb24d9b66f42604b55e4d83bd2
sekulicd <sekula87@gmail.com>
2022-11-04 17:56:22 +0100

    filter only double fields

--- internal/infrastructure/db/influx/market_balance_repository_impl_test.go
+++ internal/infrastructure/db/influx/market_balance_repository_impl_test.go
@@ -16,9 +16,9 @@ func Test_createMarkedIDsFluxQueryFilter(t *testing.T) {
 			name: "no market_id",
 			args: args{
 				marketIDs: []string{},
-				table:     "market_balance",
+				table:     "market_price",
 			},
-			want: "(r._measurement == \"market_balance\")",
+			want: "(r._measurement == \"market_price\" and (r._field == \"base_price\" or r._field == \"quote_price\"))",
 		},
 		{
 			name: "one market_id",
@@ -26,7 +26,7 @@ func Test_createMarkedIDsFluxQueryFilter(t *testing.T) {
 				marketIDs: []string{"2"},
 				table:     "market_balance",
 			},
-			want: "(r._measurement == \"market_balance\" and r.market_id==\"2\")",
+			want: "(r._measurement == \"market_balance\" and r.market_id==\"2\" and (r._field == \"base_balance\" or r._field == \"quote_balance\"))",
 		},
 		{
 			name: "multiple market_ids",
@@ -34,7 +34,15 @@ func Test_createMarkedIDsFluxQueryFilter(t *testing.T) {
 				marketIDs: []string{"2", "3", "4"},
 				table:     "market_balance",
 			},
-			want: "(r._measurement == \"market_balance\" and r.market_id==\"2\") or (r._measurement == \"market_balance\" and r.market_id==\"3\") or (r._measurement == \"market_balance\" and r.market_id==\"4\")",
+			want: "(r._measurement == \"market_balance\" and r.market_id==\"2\" and (r._field == \"base_balance\" or r._field == \"quote_balance\")) or (r._measurement == \"market_balance\" and r.market_id==\"3\" and (r._field == \"base_balance\" or r._field == \"quote_balance\")) or (r._measurement == \"market_balance\" and r.market_id==\"4\" and (r._field == \"base_balance\" or r._field == \"quote_balance\"))",
+		},
+		{
+			name: "multiple market_ids, price table",
+			args: args{
+				marketIDs: []string{"2", "3", "4"},
+				table:     "market_price",
+			},
+			want: "(r._measurement == \"market_price\" and r.market_id==\"2\" and (r._field == \"base_price\" or r._field == \"quote_price\")) or (r._measurement == \"market_price\" and r.market_id==\"3\" and (r._field == \"base_price\" or r._field == \"quote_price\")) or (r._measurement == \"market_price\" and r.market_id==\"4\" and (r._field == \"base_price\" or r._field == \"quote_price\"))",
 		},
 	}
 	for _, tt := range tests {
