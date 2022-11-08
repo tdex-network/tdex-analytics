f322ffc014c4c39f8ca404522886099351ff2395
Dusan Sekulic <sekula87@gmail.com>
2022-11-08 13:52:08 +0100

    Hofix: default to empty timeframe & fix aggregate query
    * prevent getPricesInReferenceCurrency returning error
    
    * enable timeFrame to be 0

--- test/influx-db/market_price_repository_impl_test.go
+++ test/influx-db/market_price_repository_impl_test.go
@@ -123,3 +123,60 @@ func (idb *InfluxDBTestSuit) TestGetMarketPriceWithPagination() {
 
 	idb.Equal(5, len(marketsPrices["1"]))
 }
+
+func (idb *InfluxDBTestSuit) TestGetMarketPriceWithoutGrouping() {
+	ctx := context.Background()
+
+	for i := 0; i < 10; i++ {
+		if err := dbSvc.InsertPrice(ctx, domain.MarketPrice{
+			MarketID:   "999",
+			BasePrice:  decimal.NewFromInt(int64(50 + i)),
+			BaseAsset:  "5ac9f65c0efcc4775e0baec4ec03abdde22473cd3cf33c0419ca290e0751b225",
+			QuotePrice: decimal.NewFromInt(int64(500 + i)),
+			QuoteAsset: "6f0279e9ed041c3d710a9f57d0c02928416460c4b722ae3457a11eec381c526d",
+			Time:       time.Now(),
+		}); err != nil {
+			idb.FailNow(err.Error())
+		}
+	}
+
+	startTime := time.Date(
+		application.StartYear,
+		1,
+		1,
+		0,
+		0,
+		0,
+		0,
+		time.UTC,
+	)
+	endTime := time.Date(
+		application.StartYear,
+		12,
+		1,
+		0,
+		0,
+		0,
+		0,
+		time.UTC,
+	)
+
+	page := domain.Page{
+		Number: 1,
+		Size:   10,
+	}
+
+	marketsPrices, err := dbSvc.GetPricesForMarkets(
+		ctx,
+		startTime,
+		endTime,
+		page,
+		"",
+		[]string{"999"}...,
+	)
+	if err != nil {
+		idb.FailNow(err.Error())
+	}
+
+	idb.Equal(10, len(marketsPrices["999"]))
+}
